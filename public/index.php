c60396ef131c226b2239519fd3c5a88b66f0bde3
Baboo81 <magicbaboo@gmail.com>
2025-01-31 17:05:35 +0100

    Created : contact.php

--- public/index.php
+++ public/index.php
@@ -3,11 +3,14 @@
 require_once '../vendor/autoload.php';
 
 // Inclusion des classes nécessaires
+
+use App\Controllers\ContactController;
 use App\Core\Router;
 use App\Controllers\HomeController;
 use App\Controllers\KenkoHoController;
 use App\Controllers\KenkoWebController;
 use App\Controllers\QuiSuisJeController;
+
 
 // Initialisation du routeur
 $router = new Router();
@@ -17,6 +20,7 @@ $router->get('/', [HomeController::class, 'index']); // Page d'accueil
 $router->get('/kenko-ho', [KenkoHoController::class, 'show']);
 $router->get('/kenko-web', [KenkoWebController::class, 'show']);
 $router->get('/quiSuis-je', [QuiSuisJeController::class, 'show']);
+$router->get('/contact', [ContactController::class, 'show']);
 
 // Lancer le routage
 $router->run();
