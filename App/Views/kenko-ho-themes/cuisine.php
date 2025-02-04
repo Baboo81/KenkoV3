ef89ac5958950631c1cbacf18b7f7ae6a88e208f
Baboo81 <magicbaboo@gmail.com>
2025-02-04 15:04:01 +0100

    Added content on :cuisine.php

--- App/Views/kenko-ho-themes/cuisine.php
+++ App/Views/kenko-ho-themes/cuisine.php
@@ -11,6 +11,29 @@
                     </div>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-md-12 my-5">
+                    <div class="box rounded-5 p-4 my-5">
+                        <div class="row">
+                            <article class="col-md-6">
+                                <p class="text-muted">Pour utiliser les huilles essentielles en cuisine, il faut savoir qu'une huile chauffée au-delà de 60°C perdra ses propriétés bénéfiques.<br />Si vous souhaiter conserver touts les bénéfices d'une huile, ajoutez cette dernière lorsque la préparation sera hors du feu. Pour la quantité, nous utiliserons une pointe de cure-dents imbibée d'une petite quantité de liquide qui sera mélangée à la nouriture.<br />Néamnois il est tout à fait possible d'incorporer les huiles essentielles à chaud ou dans des préparations qui seront chauffées ; dans les pâtisseries par example, un ragout, ...</p>
+                                <div class="text-center my-4">
+                                    <h3>Guide des huiles essentielles en cuisine</h3>
+                                </div>
+                                <p class="text-muted">Téléchargez notre guide sur le H.E en cuisine pour découvrir toutes les astuces et précautions à prendre.</p>
+                            </article>
+                            <div class="col-md-6">
+                                <img class="img-fluid rounded-4" src="/../assets/img/services/Kenko-Ho/pageCuisine/cuisine.jpg" alt="Image représantant une personne qui cuisine">
+                            </div>
+                        </div>
+                        <div class="text-center my-5">
+                            <a href="/../assets/pdf/pageCuisine/cuisine.pdf" download="cuisine.pdf">
+                                <button class="button">Télécharger le PDF</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </main>
