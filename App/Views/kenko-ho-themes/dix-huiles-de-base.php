8ac527fb3726036275227dcd60cdd5979e7b1416
Baboo81 <magicbaboo@gmail.com>
2025-02-05 11:43:08 +0100

    Added content: img, text

--- App/Views/kenko-ho-themes/dix-huiles-de-base.php
+++ App/Views/kenko-ho-themes/dix-huiles-de-base.php
@@ -16,7 +16,7 @@
                     <div class="box rounded-5 p-4 my-5">
                         <div class="row d-flex align-items-center">
                             <article class="col-md-8">
-                                <div class="text-center mb-3">
+                                <div class="text-center mb-5">
                                     <h3>Le mélange : Air</h3>
                                 </div>
                                 <div class="text-start mb-3">
@@ -27,7 +27,7 @@
                                     <h4>Usages :<i class="circleA mx-3">A</i><i class="circleT mx-2">T</i></h4>
                                 </div>
                                 <div class="mb-5">
-                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>Soutient la sphère O.R.L en assainissant les voie respiratoire.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>Soutien la sphère O.R.L en assainissant les voie respiratoire.</p>
                                     <p class="text-muted my-1"><i class="mx-3">&#8226;</i>Assainissant atmosphérique.</p>
                                     <p class="text-muted my-1"><i class="mx-3">&#8226;</i>En topique, diluer avec une HV sur le torse pour ouvrir les voies respiratoires, atténuer la toux, déboucher le nez.</p>
                                     <p class="text-muted my-1"><i class="mx-3">&#8226;</i>Masque sec : 1gtte dans les mains, placez les mains sur le nez et inhaler.</p>
@@ -38,12 +38,12 @@
                                 <img class="img-fluid" src="/../assets/img/services/Kenko-Ho/pageDixHuiles/Air.jpg" alt="Image représantant une personne qui cuisine">
                             </div>
                         </div>
-                        <div class="row d-flex align-items-center">
+                        <div class="row d-flex align-items-center mb-5">
                             <div class="col-md-4 text-center">
                                 <img class="img-fluid" src="/../assets/img/services/Kenko-Ho/pageDixHuiles/MenthePoivrée.jpg" alt="Image représantant une personne qui cuisine">
                             </div>
                             <article class="col-md-8 text-md-end">
-                                <div class="text-center">
+                                <div class="text-center mb-5">
                                     <h3>La Menthe Poivrée</h3>
                                 </div>
                                 <div class="text-start my-3">
@@ -59,10 +59,23 @@
                         </div>
                         <div class="row d-flex align-items-center">
                             <article class="col-md-8">
-                                <div class="text-center">
+                                <div class="text-center mb-5">
                                     <h3>Le mélange : OnGuard</h3>
                                 </div>
-                                <p class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta natus fugiat dolor illo quibusdam voluptates voluptatem odit odio deserunt rem inventore quae eius nobis, quasi possimus omnis quo cum sequi?</p>
+                                <div class="text-start mb-3">
+                                    <h4>Composition :</h4>
+                                </div>
+                                <p class="text-muted">Orange sauvage, Clou de girofle, Cannelle de Chine, Eucalyptus, Romarin.</p>
+                                <div class="text-start my-3">
+                                    <h4>Usages :<i class="circleA mx-3">A</i><i class="circleT mx-2">T</i><i class="circleI mx-2">I</i></h4>
+                                </div>
+                                <div class="mb-5">
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>Soutien les déffenses immunitaires.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>Sur le plan émotionnel, ce mélange va aider à poser ses limites afin de protéger leur espace personnel des énergies négatives.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>En diffusion pour assainir l'air.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>En massage dilué dans une HV quand le corps faiblit.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>&#x26A0; &#9788; Photosensibilisant, dermocaustique.</p>
+                                </div>
                             </article>
                             <div class="col-md-4 text-center">
                                 <img class="img-fluid" src="/../assets/img/services/Kenko-Ho/pageDixHuiles/OnGuard.jpg" alt="Image représantant une personne qui cuisine">
@@ -73,11 +86,42 @@
                                 <img class="img-fluid" src="/../assets/img/services/Kenko-Ho/pageDixHuiles/Origan.jpg" alt="Image représantant une personne qui cuisine">
                             </div>
                             <article class="col-md-8">
-                                <div class="text-center">
+                                <div class="text-center mb-5">
                                     <h3>L'Origan</h3>
                                 </div>
-                                <p class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta natus fugiat dolor illo quibusdam voluptates voluptatem odit odio deserunt rem inventore quae eius nobis, quasi possimus omnis quo cum sequi?</p>
+                                <div class="text-start my-3">
+                                    <h4>Usages :<i class="circleA mx-3">A</i><i class="circleT mx-2">T</i><i class="circleI mx-2">I</i></h4>
+                                </div>
+                                <div class="mb-5">
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>Soutien puissant du systeme immunitaire.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>En diffusion, elle renforce le sentiment de sécurité.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>En cuisine pour arômatiser une sauce, tremper une pointe de cure dent dans le flacon et ensuite dans la sauce.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>En topique : antifongique, virrucide.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>&#x26A0; Dermocaustique à diluer impérativement.</p>
+                                </div>
                             </article>
+                        </div>
+                        <div class="row d-flex align-items-center mt-5">
+                            <article class="col-md-8">
+                                <div class="text-center mb-5">
+                                    <h3>Le mélange : ZenGest</h3>
+                                </div>
+                                <div class="text-start mb-3">
+                                    <h4>Composition :</h4>
+                                </div>
+                                <p class="text-muted">Gingembre, Menthe poivrée, Estragon, Cumin, Coriandre, Anis.</p>
+                                <div class="text-start my-3">
+                                    <h4>Usages :<i class="circleA mx-3">A</i><i class="circleT mx-2">T</i><i class="circleI mx-2">I</i></h4>
+                                </div>
+                                <div class="mb-5">
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>C'est le mélange digestif, qui va soutenir la sphère digestive à tout les niveaux.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>En massage sur le ventre pour soulager ; les crampes, ballonements.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>Sur le plan émotionnel; aide à digérer les expériences de la vie.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>En interne ; 2gttes dans de l'eau chaude en dessous de 60°C, pour améliorer la digestion après un repas copieux ou trop arrosé.</p>
+                            </article>
+                            <div class="col-md-4 text-center">
+                                <img class="img-fluid" src="/../assets/img/services/Kenko-Ho/pageDixHuiles/ZenGest.jpg" alt="Image représantant une personne qui cuisine">
+                            </div>
                         </div>
                         <div class="text-center my-5">
                             <a href="/../assets/pdf/pageDixHuilesDeBase/kitFamille.pdf" download="kitFamille.pdf">
