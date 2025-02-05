570014483b20aa3307e994c39de523124bffb9b1
Baboo81 <magicbaboo@gmail.com>
2025-02-05 10:58:59 +0100

    Added contents; txt, pdf

--- App/Views/kenko-ho-themes/dix-huiles-de-base.php
+++ App/Views/kenko-ho-themes/dix-huiles-de-base.php
@@ -15,43 +15,64 @@
                 <div class="my-5">
                     <div class="box rounded-5 p-4 my-5">
                         <div class="row d-flex align-items-center">
-                            <article class="col-md-6">
-                                <div class="text-center">
+                            <article class="col-md-8">
+                                <div class="text-center mb-3">
                                     <h3>Le mélange : Air</h3>
                                 </div>
-                                <p class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta natus fugiat dolor illo quibusdam voluptates voluptatem odit odio deserunt rem inventore quae eius nobis, quasi possimus omnis quo cum sequi?</p>
+                                <div class="text-start mb-3">
+                                    <h4>Composition :</h4>
+                                </div>
+                                <p class="text-muted">Laurier (feuille), Menth poivrée, Eucalyptus, Tea-tree, Citron, Ravintsara, Cardamome.</p>
+                                <div class="text-start my-3">
+                                    <h4>Usages :<i class="circleA mx-3">A</i><i class="circleT mx-2">T</i></h4>
+                                </div>
+                                <div class="mb-5">
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>Soutient la sphère O.R.L en assainissant les voie respiratoire.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>Assainissant atmosphérique.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>En topique, diluer avec une HV sur le torse pour ouvrir les voies respiratoires, atténuer la toux, déboucher le nez.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>Masque sec : 1gtte dans les mains, placez les mains sur le nez et inhaler.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>&#x26A0; &#9788; Photosensibilisant.</p>
+                                </div>
                             </article>
-                            <div class="col-md-6 text-center">
+                            <div class="col-md-4 text-center">
                                 <img class="img-fluid" src="/../assets/img/services/Kenko-Ho/pageDixHuiles/Air.jpg" alt="Image représantant une personne qui cuisine">
                             </div>
                         </div>
                         <div class="row d-flex align-items-center">
-                            <div class="col-md-6 text-center">
+                            <div class="col-md-4 text-center">
                                 <img class="img-fluid" src="/../assets/img/services/Kenko-Ho/pageDixHuiles/MenthePoivrée.jpg" alt="Image représantant une personne qui cuisine">
                             </div>
-                            <article class="col-md-6 text-md-end">
+                            <article class="col-md-8 text-md-end">
                                 <div class="text-center">
                                     <h3>La Menthe Poivrée</h3>
                                 </div>
-                                <p class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam qui exercitationem, optio quos deserunt tenetur vitae, non quas necessitatibus, in adipisci velit. Eum sapiente sed, nesciunt accusantium quasi voluptatibus at..</p>
+                                <div class="text-start my-3">
+                                    <h4>Usages :<i class="circleA mx-3">A</i><i class="circleT mx-2">T</i><i class="circleI mx-2">I</i></h4>
+                                </div>
+                                <div class="text-start mb-5">
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>Pour donner un coup de fouet : 1gtte directement dans la bouche.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>Pour soulager les maux de tête : 1gtte en massage sur les tempes, front et sur la nuque.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>En cas d'inconfort digestif : 1 à 2 gttes dans de l'eau.</p>
+                                    <p class="text-muted my-1"><i class="mx-3">&#8226;</i>En diffusion : 2gttes, pour retrouver; optimisme, vivacité et joie.</p>
+                                </div>
                             </article>
                         </div>
                         <div class="row d-flex align-items-center">
-                            <article class="col-md-6">
+                            <article class="col-md-8">
                                 <div class="text-center">
                                     <h3>Le mélange : OnGuard</h3>
                                 </div>
                                 <p class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta natus fugiat dolor illo quibusdam voluptates voluptatem odit odio deserunt rem inventore quae eius nobis, quasi possimus omnis quo cum sequi?</p>
                             </article>
-                            <div class="col-md-6 text-center">
+                            <div class="col-md-4 text-center">
                                 <img class="img-fluid" src="/../assets/img/services/Kenko-Ho/pageDixHuiles/OnGuard.jpg" alt="Image représantant une personne qui cuisine">
                             </div>
                         </div>
                         <div class="row d-flex align-items-center">
-                            <div class="col-md-6 text-center">
+                            <div class="col-md-4 text-center">
                                 <img class="img-fluid" src="/../assets/img/services/Kenko-Ho/pageDixHuiles/Origan.jpg" alt="Image représantant une personne qui cuisine">
                             </div>
-                            <article class="col-md-6">
+                            <article class="col-md-8">
                                 <div class="text-center">
                                     <h3>L'Origan</h3>
                                 </div>
@@ -59,7 +80,7 @@
                             </article>
                         </div>
                         <div class="text-center my-5">
-                            <a href="/../assets/pdf/pageCuisine/cuisine.pdf" download="cuisine.pdf">
+                            <a href="/../assets/pdf/pageDixHuilesDeBase/kitFamille.pdf" download="kitFamille.pdf">
                                 <button class="button">Télécharger le PDF</button>
                             </a>
                         </div>
