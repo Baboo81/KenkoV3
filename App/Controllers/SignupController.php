71f7d48b4bd23d3ed2fd70f52887fef18bd2098e
Baboo81 <magicbaboo@gmail.com>
2025-02-06 20:53:44 +0100

    form authentification

--- App/Controllers/SignupController.php
+++ App/Controllers/SignupController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Core\Controller;
+use App\Core\Database;
 
 
 class SignupController extends Controller {
@@ -15,5 +16,60 @@ class SignupController extends Controller {
         $css = 'signup.css';
         //Charger la vue et passer le titre et le style
         $this->View('signup', compact('title', 'resetCss', 'css'));
+    }
+
+    public function register() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            //Démarrer la session si elle n'est pas encore active
+            if (session_status() === PHP_SESSION_NONE) {
+                session_start();
+            }
+
+            //Vérification du token CSRF
+            if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+                die("Erreur CSRF !");
+            }
+
+            //Récupération et nettoyage des données
+            $username = trim(htmlspecialchars($_POST['username']));
+            $email = trim(htmlspecialchars($_POST['email']));
+            $password = trim($_POST['password']);
+
+            //Vérification des champs obligatoires
+            if (empty($username) || empty($email) || empty($password)) {
+                die("Tous les champs doivent être remplis !");
+            }
+
+            //Vérification de la validité de l'email
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                die("Adresse email invalide !");
+            }
+
+            //Hachage du mot de passe
+            $hachedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+            //Connexion à la DB
+            $db = Database::getInstance();
+
+            //Vérification de l'email si l'email est déjà utilisé
+            $stmt = $db->prepare("SELECT id FROM users WHERE email = ?");
+            $stmt ->execute([$email]);
+
+            if ($stmt->fetch()) {
+                die("Cet adresse est déjà utilisée !");
+            }
+
+            //Insertion d'utilisateur en base données
+            $stmt = $db->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+            $success = $stmt->execute([$username, $email, $hachedPassword]);
+
+            if ($success) {
+                //Redirection vers la page d'accueil
+                header("Location: /home");
+                exit();
+            } else {
+                die("Erreur lors de l'inscription !");
+            }
+        }
     }
 }
--- App/Views/signup.php
+++ App/Views/signup.php
@@ -6,18 +6,18 @@
             <div class="row">
                 <h1 class="text-center my-5">Inscription</h1>
                 <div class="col-md-12">
-                    <form action="/signup" method="$_POST">
+                    <form action="/signup" method="post" autocomplete="off">
                         <div class="mb-3">
                             <label for="username" class="form-label">Nom d'utilisateur :</label>
-                            <input type="text" class="form-control" id="username" name="username" required>
+                            <input type="text" class="form-control" id="username" name="username" autocomplete="off" required>
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">Mot de passe :</label>
-                            <input type="text" class="form-control" id="password" name="password" required>
+                            <input type="password" class="form-control" id="password" name="password" autocomplete="off" required>
                         </div>
                         <div class="mb-3">
                             <label for="email" class="form-label">Email :</label>
-                            <input type="email" class="form-control" id="email" name="email" required>
+                            <input type="email" class="form-control" id="email" name="email" autocomplete="off" required>
                         </div>
                         <div class="text-center my-5">
                             <button type="submit">S'inscrire</button>
