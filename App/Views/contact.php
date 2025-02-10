91d042c8836b900041c9f865b6010c7845273f46
Baboo81 <magicbaboo@gmail.com>
2025-02-10 20:25:49 +0100

    forms

--- App/Views/contact.php
+++ App/Views/contact.php
@@ -9,34 +9,37 @@
 <main>
     <section class="mainSection">
         <div class="container">
-            <div class="row">
+            <div class="row justify-content-center align-items-center">
                 <h1 class="text-center mb-5">Formulaire de contact</h1>
-                <div class="col-md-12">
-                    <form action="/contact" method="POST">
+                <div class="col-md-6">
+                    <form class="border rounded-5 p-5" action="/contact" method="POST">
                         <div class="mb-3">
-                            <label for="name" class="form-label">Nom</label>
+                            <label for="name" class="form-label mb-3">Nom</label>
                             <input type="text" class="form-control" id="name" name="name" required>
                         </div>
                         <div class="mb-3">
-                            <label for="firstName" class="form-label">Prénom</label>
+                            <label for="firstName" class="form-label mb-3">Prénom</label>
                             <input type="text" class="form-control" id="firstName" name="firstName" required>
                         </div>
                         <div class="mb-3">
-                            <label for="email" class="form-label">Email</label>
+                            <label for="email" class="form-label mb-3">Email</label>
                             <input type="email" class="form-control" id="email" name="email" required> 
                         </div>
                         <div class="mb-3">
-                            <label for="message" class="form-label">Message</label>
+                            <label for="message" class="form-label mb-3">Message</label>
                             <textarea class="form-control" id="message" name="message" required></textarea>
                         </div>
-                        <div class="mb-3">
-                            <button type="submit" class="btn btn-primary">Envoyer</button>
+                        <div class="text-center my-5">
+                            <button type="submit" class="button">Envoyer</button>
                         </div>
                     </form>
+                    <div class="text-center my-5">
+                        <a href="/">
+                            <button class="button">Retour vers la page d'accueil</button>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
 </main>
-
-<?php include 'partials/footer.php'; ?>
