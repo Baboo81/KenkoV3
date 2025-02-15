cb4f6c85e2e501297b64af4cdf2c024352390b0f
Baboo81 <magicbaboo@gmail.com>
2025-02-15 13:56:57 +0100

    debug

--- App/Views/login.php
+++ App/Views/login.php
@@ -14,18 +14,25 @@
 
             <div class="row justify-content-center align-items-center">
                 <h1 class="text-center text-muted mb-5">Login</h1>
-                <div class="col-md-6 my-5">
-                    <form class="border rounded-5 p-5" action="/login" method="post">
-                        <div class="mb-3">
-                            <label for="email" class="form-label mb-3">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" placeholder="Votre adresse email" required> 
+                <div class="col-md-12 my-5">
+                    <form class="border rounded-5 p-5 d-flex align-center justify-items-center" action="/login" method="post">
+                        <div class="col-md-6 d-flex align-items-center justify-content-center">
+                            <img src="/assets/img/form/login.svg" alt="Illustration contact" class="img-fluid w-100" style="object-fit:cover;">
                         </div>
-                        <div class="mb-5">
-                            <label for="password" class="form-label mb-3">Mot de passe</label>
-                            <input type="password" class="form-control" id="password" name="password" placeholder="Mot de passe" required> 
-                        </div>
-                        <div class="text-center">
-                            <button type="submit" class="button">Se connecter</button>
+                        <div class="col-md-6 my-5">
+                            <section class="my-5">
+                                <div class="mb-3">
+                                    <label for="email" class="form-label mb-3">Email</label>
+                                    <input type="email" class="form-control" id="email" name="email" placeholder="Votre adresse email" required> 
+                                </div>
+                                <div class="mb-5">
+                                    <label for="password" class="form-label mb-3">Mot de passe</label>
+                                    <input type="password" class="form-control" id="password" name="password" placeholder="Mot de passe" required> 
+                                </div>
+                                <div class="text-center">
+                                    <button type="submit" class="button">Se connecter</button>
+                                </div>
+                            </section>
                         </div>
                     </form>
                     <div class="text-center my-5">
