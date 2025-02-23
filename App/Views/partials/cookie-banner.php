bfb2c8c63e1bdb9f24b094ca1af9398d95d3272e
Baboo81 <magicbaboo@gmail.com>
2025-02-23 14:09:21 +0100

    Kenko

--- App/Views/partials/cookie-banner.php
+++ App/Views/partials/cookie-banner.php
@@ -1,12 +1,13 @@
 <div class="container">
-    <div class="row">
+    <div class="row justify-content-center">
         <?php if (!isset($_COOKIE['cookie-preferences'])): ?>
         <div id="cookie-banner" class="cookie-banner alert alert-dark text-center position-fixed bottom-0" role="alert">
             <div class="cookie-icon-container">
-                <img src="./assets/img/icons/cookiesIcon.svg" alt="Picto représentant un cookie">
+                <img src="./assets/img/icons/cookiesIcon.svg" alt="Picto représentant un cookie" style="height: 10rem;">
             </div>
-            <article>
-                <p>Nous utilisons des cookies pour améliorer votre expérience sur notre site !En poursuivant votre navigation, vous acceptez notre <a href="politique-cookies.php" classe="
+            <article class="my-5">
+                <p class="text-muted">Nous utilisons des cookies pour améliorer votre expérience sur notre site !</p>
+                <p class="text-muted">En poursuivant votre navigation, vous acceptez notre <a href="politique-cookies.php" classe="
                 text-decoration-underline">politiques de cookies</a>.</p>
             </article>
             <div>
