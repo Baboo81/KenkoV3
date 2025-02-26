94f24bf77619f2cd619a12d0747b5bfbe210f0d1
Baboo81 <magicbaboo@gmail.com>
2025-02-26 15:48:53 +0100

    Modif

--- app/views/quiSuis-je.php
+++ app/views/quiSuis-je.php
@@ -14,26 +14,47 @@
             </section>
             <section class="my-5">
                 <div class="container">
-                    <h1 class="text-center my-5 fs-1 fontBlack">Devenir webmaster m'a permis de regrouper plusieurs passions</h1>
-                    <div class="row">
-                        <div class="my-5 d-flex">
-                            <article class="col-md-3 text-center">
-                                <img src="/assets/img/services/quiSuis-je/informatique.svg" alt="Picto représentant un ordinateur">
-                                <h4 class="fs-4 my-1">L'informatique</h4>
-                            </article>
-                            <article class="col-md-3 text-center">
-                                <img src="/assets/img/services/quiSuis-je/design.svg" alt="Picto représentant l'outil plume dans illustrator">
-                                <h4 class="fs-4 my-1">Le design</h4>
-                            </article>
-                            <article class="col-md-3 text-center">
-                                <img src="/assets/img/services/quiSuis-je/photo.svg" alt="Picto représentant un appareil photo">
-                                <h4 class="fs-4 my-5">La photo</h4>
-                            </article>
-                            <article class="col-md-3 text-center">
-                                <img src="/assets/img/services/quiSuis-je/communication.svg" alt="Picto représentant un personnage relié à d'autres personnes">
-                                <h4 class="fs-4">La communication</h4>
-                            </article>
-                        </div>
+                    <div class="my-5">
+                        <h1 class="text-center my-5 fs-1 fontBlack">Devenir webmaster m'a permis de regrouper plusieurs passions</h1>
+                    </div>
+                    <div class="row text-center my-5 pictoBloc">
+                        <article class="col-6 col-md-3 d-flex flex-column align-items-center">
+                            <img src="/assets/img/services/quiSuis-je/informatique.svg" alt="Picto représentant un ordinateur" class="img-fluid" style="max-width: 120px;">
+                            <h4 class="fs-4 my-3">L'informatique</h4>
+                        </article>
+                        <article class="col-6 col-md-3 d-flex flex-column align-items-center">
+                            <img src="/assets/img/services/quiSuis-je/design.svg" alt="Picto représentant l'outil plume dans illustrator" class="img-fluid" style="max-width: 120px;">
+                            <h4 class="fs-4 my-3">Le design</h4>
+                        </article>
+                        <article class="col-6 col-md-3 d-flex flex-column align-items-center">
+                            <img src="/assets/img/services/quiSuis-je/photo.svg" alt="Picto représentant un appareil photo" class="img-fluid" style="max-width: 139px;">
+                            <h4 class="fs-4 my-3">La photo</h4>
+                        </article>
+                        <article class="col-6 col-md-3 d-flex flex-column align-items-center">
+                            <img src="/assets/img/services/quiSuis-je/communication.svg" alt="Picto représentant un personnage relié à d'autres personnes" class="img-fluid" style="max-width: 106px;">
+                            <h4 class="fs-4 my-3">La communication</h4>
+                        </article>
+                    </div>
+                    <div class="my-5">
+                        <h1 class="text-center my-5 fs-1 fontBlack">Devenir conseillère bien-être m'a permis de regrouper d'autres passions</h1>
+                    </div>
+                    <div class="row text-center my-5 pictoBloc">
+                        <article class="col-6 col-md-3 d-flex flex-column align-items-center">
+                            <img src="/assets/img/services/quiSuis-je/santé.svg" alt="Picto représentant un ordinateur" class="img-fluid" style="max-width: 120px;">
+                            <h4 class="fs-4 my-3">La santé</h4>
+                        </article>
+                        <article class="col-6 col-md-3 d-flex flex-column align-items-center">
+                            <img src="/assets/img/services/quiSuis-je/reiki.svg" alt="Picto représentant l'outil plume dans illustrator" class="img-fluid" style="max-width: 136px;">
+                            <h4 class="fs-4 my-3">Le Reiki</h4>
+                        </article>
+                        <article class="col-6 col-md-3 d-flex flex-column align-items-center">
+                            <img src="/assets/img/services/quiSuis-je/aroma.svg" alt="Picto représentant un appareil photo" class="img-fluid" style="max-width: 73px;">
+                            <h4 class="fs-4 my-3">L'aromathérapie</h4>
+                        </article>
+                        <article class="col-6 col-md-3 d-flex flex-column align-items-center">
+                            <img src="/assets/img/services/quiSuis-je/coaching.svg" alt="Picto représentant un personnage relié à d'autres personnes" class="img-fluid" style="max-width: 106px;">
+                            <h4 class="fs-4 my-3">Le coaching</h4>
+                        </article>
                     </div>
                 </div>
             </section>
@@ -41,9 +62,10 @@
                 <div class="container">
                     <div class="row">
                         <h1 class="text-center fs-1 fontBlack my-5 p-5">Mon parcours en quelques mots</h1>
-                        <article class="col-md-12 my-5 p-5">
+                        <article class="col-md-8 my-5 p-5">
                             <div class="box rounded-5 p-4">
                                 <p class="text-muted">Après 15 années passées en pharmacie, j'ai décidé de réorienter ma carrière professionnelle pour aller vers une passion de longue date; l'informatique.<br />Après m'être formée au développement web, j'ai décidé de me lancer comme webmaster freelance.</p>
+                                <p class="text-muted">La découverte de dōTERRA il y a quelques années et ma passion pour la santé m'ont donnés envie d'aider les personnes à retrouver leur bien-être avec d'une part les huiles essentielles et d'autre part le Reiki.</p>
                             </div>
                         </article>
                     </div>
