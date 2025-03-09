61de19fb077b42d47625bc6ca27c50b326eb61b4
Baboo81 <magicbaboo@gmail.com>
2025-03-09 19:59:05 +0100

    Cookie-banner

--- App/Views/partials/cookie-banner.php
+++ App/Views/partials/cookie-banner.php
@@ -1,9 +1,9 @@
 <div class="container">
     <div class="row justify-content-center">
         <?php if (!isset($_COOKIE['cookie-preferences'])): ?>
-        <div id="cookie-banner" class="cookie-banner alert alert-dark text-center position-fixed bottom-0" role="alert">
+        <div id="cookie-banner" class="cookie-banner alert alert-dark text-center position-fixed bottom-0 col-md-6" role="alert">
             <div class="cookie-icon-container">
-                <img src="./assets/img/icons/cookiesIcon.svg" alt="Picto représentant un cookie" style="height: 10rem;">
+                <img src="./assets/img/icons/cookiesIcon.svg" alt="Picto représentant un cookie" style="height: 15rem;">
             </div>
             <article class="my-5">
                 <p class="text-muted">Nous utilisons des cookies pour améliorer votre expérience sur notre site !</p>
