3cbc604762a5a51d75e96c3831b8f6bd37acdddb
Baboo81 <magicbaboo@gmail.com>
2025-03-18 09:51:12 +0100

    politique des cookies

--- App/Views/cookies/politiquesCookies.php
+++ App/Views/cookies/politiquesCookies.php
@@ -1,38 +1,54 @@
-<?php include __DIR__ .'/../partials/header.php'; ?>
+<?php include __DIR__ . '/../partials/header.php'; ?>
+<?php include __DIR__ . '/../partials/navBar.php'; ?>
 
 <main>
-    <div class="container">
+    <div class="container mainContent">
         <div class="row">
-            <h1 class="text-center mb-4">Politique de Cookies</h1>
-            <div class="col-md-6 my-5">
+            <div class="my-5">
+                <h1 class="text-center text-muted my-5">Politique des Cookies</h1>
+            </div>
+            <div class="col-md-8 my-5">
                 <div class="box rounded-5 p-5">
-                    <p><strong>Dernière mise à jour :</strong> 17/03/2025</p>
-    
-                    <h2>Qu’est-ce qu’un cookie ?</h2>
-                    <p>Un cookie est un petit fichier texte stocké sur votre appareil...</p>
+                    <p class="text-muted text-center mb-3"><strong>Dernière mise à jour :</strong> 17/03/2025</p>
+                    <article class="my-4">
+                        <h2 class="my-3">Qu’est-ce qu’un cookie ?</h2>
+                        <p class="text-muted">Un cookie est un petit fichier texte stocké sur votre appareil. Ce cookie servira à mémoriser certaines informations sur l'utilisateur, voici les informations que les cookies peuvent stocker :</p>
+                        <ul class="text-muted my-3">
+                            <li class="mx-4">1. Les préférences utilisateur du type : thème sombre, langue, etc</li>
+                            <li class="mx-4">2. Les identifiants de connexion (afin d'éviter de se reconnecter à chaque visite)</li>
+                            <li class="mx-4">3. Le panier d'achat sur un site e-commerce</li>
+                            <li class="mx-4">4. Le comportement de navigation</li>
+                        </ul>
+                    </article>
+                    <article class="my-4">
+                        <h2 class="my-3">Quels types de cookies utilisons-nous ?</h2>
+                        <h3 class="my-3"> Cookies Essentiels</h3>
+                        <p class="text-muted">Ces cookies sont nécessaires...</p>
 
-                    <h2>Quels types de cookies utilisons-nous ?</h2>
-                    <h3> Cookies Essentiels</h3>
-                    <p>Ces cookies sont nécessaires...</p>
+                        <h3 class="my-3">Cookies Analytiques</h3>
+                        <p class="text-muted">Ces cookies nous aident à comprendre...</p>
 
-                    <h3>Cookies Analytiques</h3>
-                    <p>Ces cookies nous aident à comprendre...</p>
+                        <h3 class="my-3">Cookies Marketing</h3>
+                        <p class="text-muted">Ces cookies permettent d’afficher...</p>
+                    </article>
+                    <article>
+                        <h2>Comment gérer vos préférences ?</h2>
+                        <p>Vous pouvez modifier vos préférences en cliquant ci-dessous.</p>
+                        <div class="text-center mt-auto">
+                            <button onclick="window.location.href='/kenko-ho';" class="button">Gérer vos préférences</button>
+                        </div>
+                    </article>
+                    <article>
+                        <h2>Durée de conservation des cookies</h2>
+                        <p>Nous conservons vos préférences pendant...</p>
 
-                    <h3>Cookies Marketing</h3>
-                    <p>Ces cookies permettent d’afficher...</p>
-
-                    <h2>Comment gérer vos préférences ?</h2>
-                    <p>Vous pouvez modifier vos préférences <a href="cookie-preferences.php">ici</a>.</p>
-
-                    <h2>Durée de conservation des cookies</h2>
-                    <p>Nous conservons vos préférences pendant...</p>
-
-                    <h2>Contact</h2>
-                    <p>Pour toute question, contactez-nous à <a href="mailto:tonemail@example.com">tonemail@example.com</a>.</p>
+                        <h2>Contact</h2>
+                        <p>Pour toute question, contactez-nous à <a href="mailto:kenko.com">kenko@webmaster.com</a>.</p>
+                    </article>
                 </div>
             </div>
-            <div class="col-md-6">
-                <img src="/assets/img/img/cookies.jpg" alt="Photo représentant une pile de cookies" style="height : 20rem">
+            <div class="col-md-4">
+                <img src="/assets/img/img/cookies.jpg" class="img-fluid rounded-5" alt="Photo représentant une pile de cookies" style="height : 20rem">
             </div>
         </div>
     </div>
