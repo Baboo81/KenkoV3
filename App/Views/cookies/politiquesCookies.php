e2b9e628e5053792a8ff66ca98a3cedc224d9a04
Baboo81 <magicbaboo@gmail.com>
2025-03-18 15:34:37 +0100

    politique des cookies

--- App/Views/cookies/politiquesCookies.php
+++ App/Views/cookies/politiquesCookies.php
@@ -17,38 +17,49 @@
                             <li class="mx-4">1. Les préférences utilisateur du type : thème sombre, langue, etc</li>
                             <li class="mx-4">2. Les identifiants de connexion (afin d'éviter de se reconnecter à chaque visite)</li>
                             <li class="mx-4">3. Le panier d'achat sur un site e-commerce</li>
-                            <li class="mx-4">4. Le comportement de navigation</li>
+                            <li class="mx-4">4. Le comportement de navigation.</li>
                         </ul>
                     </article>
                     <article class="my-4">
                         <h2 class="my-3">Quels types de cookies utilisons-nous ?</h2>
-                        <h3 class="my-3"> Cookies Essentiels</h3>
-                        <p class="text-muted">Ces cookies sont nécessaires...</p>
-
-                        <h3 class="my-3">Cookies Analytiques</h3>
-                        <p class="text-muted">Ces cookies nous aident à comprendre...</p>
-
-                        <h3 class="my-3">Cookies Marketing</h3>
-                        <p class="text-muted">Ces cookies permettent d’afficher...</p>
+                        <h3 class="text-center my-4">Les cookies analytiques</h3>
+                        <p class="text-muted">Ces cookies nous aident à comprendre comment les visiteurs interagisssent avec notre site. Ils collectent des données sur :</p>
+                        <ul class="text-muted my-3">
+                            <li class="mx-4">1. Le nombre de visites et les pages consultées</li>
+                            <li class="mx-4">2. La durée de navigation sur le site</li>
+                            <li class="mx-4">3. Les sources du trafic (moteurs de recherche, réseaux sociaux, etc.)</li>
+                        </ul>
+                        <p class="text-muted">Ces informations nous permettent d'améliorer nos contenus et l'espérience utilisateur.</p>
+                        <h3 class="text-center my-4">Les cookies marketing</h3>
+                        <p class="text-muted">Ces cookies sont utilisés pour personnaliser les publicités affichées sur notre site et ailleurs sur le web. Ils permettent :</p>
+                        <ul class="text-muted my-3">
+                            <li class="mx-4">1. D'afficher des publicités en fonctions de vos centres d'intérêts</li>
+                            <li class="mx-4">2. De limiter le nombre de fois où vous verrez une publicité</li>
+                            <li class="mx-4">3. De mesurer l'efficacité des campagnes publicitaires</li>
+                        </ul>
+                        <p class="text-muted">Vous pouver <b>accepter ou refuser</b> ces cookies via notre bannière de gestion des préférences.</p>
                     </article>
                     <article>
-                        <h2>Comment gérer vos préférences ?</h2>
-                        <p>Vous pouvez modifier vos préférences en cliquant ci-dessous.</p>
-                        <div class="text-center mt-auto">
-                            <button onclick="window.location.href='/kenko-ho';" class="button">Gérer vos préférences</button>
+                        <h2 class="my-3">Comment gérer vos préférences ?</h2>
+                        <p class="text-muted">Vous pouvez modifier vos préférences en cliquant ci-dessous.</p>
+                        <div class="text-center my-4">
+                            <button onclick="window.location.href='/cookies/cookie-preferences';" class="button">Gérer vos préférences</button>
                         </div>
                     </article>
                     <article>
-                        <h2>Durée de conservation des cookies</h2>
-                        <p>Nous conservons vos préférences pendant...</p>
+                        <h2 class="my-3">Durée de conservation des cookies</h2>
+                        <p class="text-muted">Nous conservons vos préférences pendant <b>13 mois</b>.</p>
 
-                        <h2>Contact</h2>
-                        <p>Pour toute question, contactez-nous à <a href="mailto:kenko.com">kenko@webmaster.com</a>.</p>
+                        <h2 class="my-3">Contact</h2>
+                        <p class="text-muted">Pour toute question, contactez-moi en cliquant ci-dessous.</p>
+                        <div class="text-center my-4">
+                            <button onclick="window.location.href='/contact';" class="button">Contactez-moi</button>
+                        </div>
                     </article>
                 </div>
             </div>
-            <div class="col-md-4">
-                <img src="/assets/img/img/cookies.jpg" class="img-fluid rounded-5" alt="Photo représentant une pile de cookies" style="height : 20rem">
+            <div class="col-md-4 position">
+                <img src="/assets/img/img/cookies.jpg" class="img-fluid rounded-5 cookie-image" alt="Photo représentant une pile de cookies">
             </div>
         </div>
     </div>
