3f2d53201b53b0a4f680bdee7eb264ecc991ee3e
Baboo81 <magicbaboo@gmail.com>
2025-03-19 17:45:22 +0100

    Responsive : footer

--- App/Views/partials/footer.php
+++ App/Views/partials/footer.php
@@ -2,29 +2,29 @@
             <section class="">
               <div class="container text-center text-md-start mt-5" id="containerFooter">
                 <div class="row mt-3">
-                  <div class="col-md-2 col-lg-4 col-xl-3 mx-auto mb-4">
-                    <p class="text-uppercase text-center fontWhite fw-bold mb-4">
+                  <div class="col-md-12 col-xs-12 text-center mb-4">
+                    <p class="text-uppercase text-center fontWhite fw-bold fs-2 spaceLetter mb-5">
                       <i class="fas fa-gem me-3"></i>Adresse
                     </p>
-                    <p class="text-center fontWhite">
+                    <p class="fontWhite fs-4 my-5">
                         Rue champ rodange 14, Waterloo 1410
                     </p>
-                    <div id="mapid" class="container-fluid my-3"></div>
+                    <div id="mapid" class="container-fluid rounded-5 my-3"></div>
                   </div>
-                  <div class="col-md-4 col-lg-2 col-xl-2 mx-auto mb-4">
-                    <p class="text-uppercase text-center fontWhite fw-bold mb-4">
+                  <div class="col-md-12 col-xs-12 text-center mb-5">
+                    <p class="text-uppercase text-center fontWhite fw-bold spaceLetter fs-2 mb-5">
                       Horaires
                     </p>
-                    <div class="text-center fontWhite">
-                        <p>Lundi    09h00 - 17h00</p>
-                        <p>Mardi    09h00 - 17h00</p>
-                        <p>Mercredi 09h00 - 17h00</p>
-                        <p>Jeudi    09h00 - 17h00</p>
+                    <div class="text-center fontWhite fs-4">
+                        <p class="mb-3">Lundi    09h00 - 17h00</p>
+                        <p class="mb-3">Mardi    09h00 - 17h00</p>
+                        <p class="mb-3">Mercredi 09h00 - 17h00</p>
+                        <p class="mb-3">Jeudi    09h00 - 17h00</p>
                         <p>Vendredi 09h00 - 17h00</p>
                     </div>
                   </div>
-                  <div class="col-md-4 col-lg-2 col-xl-2 mx-auto mb-4">
-                        <p class="text-uppercase text-center fontWhite fw-bold mb-4">
+                  <div class="col-md-12 col-xs-12 text-center mb-4">
+                        <p class="text-uppercase fontWhite fw-bold fs-2 spaceLetter mb-5">
                             Réseaux Sociaux
                         </p>
                         <div class="text-center">
@@ -33,18 +33,18 @@
                             <a href="https://www.linkedin.com/in/chris-rodriguez-perez-609716100/"><img class="icons" src="/assets/img/icons/linkedin.svg" alt="Icône Instagram"></a>
                         </div>
                   </div>
-                  <div class="col-md-2 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                    <p class="text-uppercase text-center fontWhite fw-bold mb-4">Coordonnées</p>
-                    <div class="text-center">
-                        <p class="text-center fontWhite">Envoyez-moi un mail en cliquant ci-dessous</p>
+                  <div class="col-md-12 col-xs-12 text-center mb-4">
+                    <p class="text-uppercase fontWhite fw-bold fs-2 spaceLetter mb-5">Coordonnées</p>
+                    <div class="">
+                        <p class="text-center fontWhite fs-3">Envoyez-moi un mail en cliquant ci-dessous</p>
                         <a href="mailto:chrisrodriguez@hotmail.be"><img class="icons" src="/assets/img/icons/email.svg" alt="Icône email"></a> 
                     </div>
-                    <p class="text-center fontWhite">0470/35.05.87</p>
+                    <p class="text-center fontWhite fs-3">0470/35.05.87</p>
                   </div>
                 </div>
               </div>
             </section>
-                <div class="text-center fontWhite fw-bolder my-5">
+                <div class="text-center fontWhite fw-bolder mt-5">
                     <p>&copy; Kenko - 2025</p>
                 </div>
           </footer>
