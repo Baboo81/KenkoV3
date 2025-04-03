78722cd12f1df3d765c24c77d5442155fb115db1
Baboo81 <magicbaboo@gmail.com>
2025-04-03 11:27:25 +0200

    FAQ

--- App/Views/foireAuxQuestions.php
+++ App/Views/foireAuxQuestions.php
@@ -26,89 +26,94 @@
                                 <div class="accordion-item">
                                     <h2 class="accordion-header" id="headingOne">
                                         <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                            Quels services propose Kenko-Web ?
+                                           <h4>Quels services propose Kenko-Web ?</h4>
                                         </button>
                                     </h2>
                                 <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
-                                        <strong>Kenko-Web pour propose un service de création de site web.</strong> La création d'un site web, repose tout d'abord sur la création d'une maquette. La maquette c'est une reproduction du visuel du site, grâce à cela vous aurez l'aspect visuel du site, cela va permettre de se mettre d'accord sur ce que souhaite le client. Nous mettrons en place la charte graphique à ce moment là.
+                                        <p><strong>Kenko-Web pour propose un service de création de site web.</strong> La création d'un site web, repose tout d'abord sur la création d'une maquette. La maquette c'est une reproduction du visuel du site, grâce à cela vous aurez l'aspect visuel du site, cela va permettre de se mettre d'accord sur ce que souhaite le client. Nous mettrons en place la charte graphique à ce moment là.
+                                        </p>
                                     </div>
                                 </div>
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="headingTwo">
                                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                        Puis-je avoir un site sur-mesure ?
+                                      <h4>Puis-je avoir un site sur-mesure ?</h4>
                                     </button>
                                 </h2>
                                 <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
-                                        <strong>Votre webmaster vous propose un site sur-mesure.</strong> Votre site sera conforme avec vos souhaits dans la mesure du raisonnable bien-entendu.
+                                        <p><strong>Votre webmaster vous propose un site sur-mesure.</strong> Votre site sera conforme avec vos souhaits dans la mesure du raisonnable bien-entendu.
+                                        </p>
                                     </div>
                                 </div>
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="headingThree">
                                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                        Est-ce qu'une refonte serait possible ? 
+                                       <h4>Est-ce qu'une refonte serait possible ?</h4> 
                                     </button>
                                 </h2>
                                 <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
-                                        <strong>La refonte de site est possible.</strong> Je précise néanmoins que je travail sans CMS du moins pour le moment, ce qui fait que je déveloperais le nouveau site en utilisant les technologies que j'utilisent habituellement.
+                                       <p><strong>La refonte de site est possible.</strong> Je précise néanmoins que je travail sans CMS du moins pour le moment, ce qui fait que je déveloperais le nouveau site en utilisant les technologies que j'utilisent habituellement.
+                                       </p>
                                     </div>
                                 </div>
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="headingFour">
                                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                                        Quelles technologies utilise votre webmaster ? 
+                                       <h4>Quelles technologies utilise votre               <strong>webmaster</strong> ?</h4> 
                                     </button>
                                 </h2>
                                 <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
-                                       Votre webmaster utilise les technologies suivantes : CSS, HTML, PHP, JavaScript. Pour la partie graphique (création : du logo, d'icônes, dessins, ... ) Adobe Illustrator sera utilisé.
+                                       <p>Votre <strong>webmaster</strong> utilise les technologies suivantes : CSS, HTML, PHP, JavaScript. Pour la partie graphique (création : du logo, d'icônes, dessins, ... ) Adobe Illustrator sera utilisé.
+                                       </p>
                                     </div>
                                 </div>
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="headingFive">
                                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-                                        Est-ce que le devis sera payant ? 
+                                       <h4>Est-ce que le devis sera payant ?</h4> 
                                     </button>
                                 </h2>
                                 <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
-                                       Le devis sera gratuit.
+                                       <p>Le devis sera gratuit.</p>
                                     </div>
                                 </div>
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="headingSix">
                                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
-                                        Est-ce que le SEO (référencement du site) sera optimiser ? 
+                                        <h4>Est-ce que le <strong>SEO (référencement du site) </strong> sera optimiser ?</h4>   
                                     </button>
                                 </h2>
                                 <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
-                                       Un référencement de qualité sera mis en place.
+                                       <p>Un référencement de qualité sera mis en place.</p>
                                     </div>
                                 </div>
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="headingSeven">
                                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
-                                        Quels sont les délais pour la création d'un site web ? 
+                                      <h4> Quels sont les délais pour la création d'un site web ?</h4>  
                                     </button>
                                 </h2>
                                 <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
-                                       Le délai de création d'un site web dépend de plusieurs facteurs, notamment la complexité du projet, et des fonctionnalités souhaités et de la réactivité du client pour valider les étapes.
+                                        <p>Le délai de création d'un site web dépend de plusieurs facteurs, notamment la complexité du projet, et des fonctionnalités souhaités et de la réactivité du client pour valider les étapes.
+                                        </p>
                                        <ul class="text-start my-3">
-                                            <li>@ Pour un site vitrine simple (1 à 5 pages) : 2 à 4 semaines.</li>
-                                            <li class="my-3">@ Pour un site avec des fonctionnalités spécifiques (blog, formulaire avancés, ...) 4 à 8 semaines.</li>
-                                            <li class="my-3">@ Pour un site e-commerce(boutique en ligne avec paiement, gestion de stock, ...) 6 à 12 semaines.</li>
-                                            <li class="my-3">@ Site sur-mesure (dévelopement spécifique, intégrations complexes, ...) 3 à 6 mois.</li>
+                                            <li>- Pour un site vitrine simple (1 à 5 pages) : 2 à 4 semaines.</li>
+                                            <li class="my-3">- Pour un site avec des fonctionnalités spécifiques (blog, formulaire avancés, ...) 4 à 8 semaines.</li>
+                                            <li class="my-3">- Pour un site e-commerce(boutique en ligne avec paiement, gestion de stock, ...) 6 à 12 semaines.</li>
+                                            <li class="my-3">- Site sur-mesure (dévelopement spécifique, intégrations complexes, ...) 3 à 6 mois.</li>
                                        </ul>
                                     </div>
                                 </div>
@@ -123,12 +128,12 @@
                                 <div class="accordion-item">
                                     <h2 class="accordion-header" id="headingOne">
                                         <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                            Accordion Item #1
+                                            Comment accéder à la plateforme Kenko-Ho ?
                                         </button>
                                     </h2>
                                 <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
-                                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                       <p>Pour accéder à la plateforme Kenko-Ho, il faut être suivi par un <strong>dōTERRA et plus précisement par Christel Rodriguez Perez</strong>. Pour plus d'informations à ce sujet, contactez-moi via le formulaire de contact se trouvant sur la page : contact ou par email et par téléphone aussi, toutes les coordonnées se trouvent en-bas de chaque pages du site.</p>
                                     </div>
                                 </div>
                             </div>
