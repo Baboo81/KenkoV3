5d90ed4b60dc946522276089e751b3bff57aea1c
Baboo81 <magicbaboo@gmail.com>
2025-04-08 15:24:50 +0200

    Added content : Reiki.php

--- App/Views/kenko-ho-themes/reiki.php
+++ App/Views/kenko-ho-themes/reiki.php
@@ -18,8 +18,8 @@
                         <div class="row">
                             <article class="col-md-6">
                                 <h3 class="text-center my-4">Qu'est-ce que le Reiki ?</h3>
-                                <p class="text-muted">Le Reiki est une méthode de soin énergétique d'origine japonaise.<br />Le Reiki est une pratique énergétique mais aussi une approche de développement spirituelle.</p>
-                                <p class="text-muted">Son fondateur ; Mikao Usui invitait ses élèves à appliquer 5 préceptes "Gokai" en japonais. Voici les 5 préceptes :
+                                <p class="text-muted mb-3">Le Reiki est une méthode de soin énergétique d'origine japonaise.<br />Le Reiki est une pratique énergétique mais aussi une approche de développement spirituelle.</p>
+                                <p class="text-muted my-3">Son fondateur ; Mikao Usui invitait ses élèves à appliquer 5 préceptes "Gokai" en japonais. Voici les 5 préceptes :
                                 </p>
                                 <ul>
                                     <li>Juste pour aujourd'hui, je me libère de toute préocupation,</li>
@@ -31,9 +31,11 @@
                                 <div class="text-center my-4">
                                     <h3>Histoire du Reiki</h3>
                                 </div>
-                                <p class="text-muted">L'histoire du Reiki a commencé il y a près d'une centaine d'années au Japon grâce à Mikao Usui Sensei qui naquit le 15 août 1865. C'était un personnage relativement singulier, il est né dans une famille de samouraïs, même si ces derniers étaient déjà interdits à l'époque de son adolescence.<br />Une telle tradition ne s'oublie pas du jour au lendemain, probablement grâce à cela, dés sa prime jeunesse, il a commencé à s'intéresser à l'utilisation des énergies d'une façon générale et à la spiritualité.</p>
-                                <p class="text-muted">Vers l'âge de huit ans, il a commencé à pratiquer le Kiko (Qi Gong version japonaise). Au bout de 2 ou 3 ans, il acquiert des capacité énergétiques surprenantes et il peut déjà traiter d'autres personnes avec cette énergie.<br />Il continue son éducation à l'école et en même temps fait des études dans différents domaines. Il sera naturellement attiré par les arts martiaux et grâce à cela il apprendra beaucoup à propos de l'utilisation de l'énergie, du Ki.</p>
-                                <p class="text-muted">L'apprentissage spirituele et énergétique de Mikao Usui Sensei a été grandement influencé par 4 voies : le Shintoïsme, le Bouddhisme, les pratiques samouraïs et le Shugendo.</p>
+                                <p class="text-muted mb-3">L'histoire du Reiki a commencé il y a près d'une centaine d'années au Japon grâce à Mikao Usui Sensei qui naquit le 15 août 1865. C'était un personnage relativement singulier, il est né dans une famille de samouraïs, même si ces derniers étaient déjà interdits à l'époque de son adolescence.<br />Une telle tradition ne s'oublie pas du jour au lendemain, probablement grâce à cela, dés sa prime jeunesse, il a commencé à s'intéresser à l'utilisation des énergies d'une façon générale et à la spiritualité.</p>
+                                <p class="text-muted mb-3">Vers l'âge de huit ans, il a commencé à pratiquer le Kiko (Qi Gong version japonaise). Au bout de 2 ou 3 ans, il acquiert des capacité énergétiques surprenantes et il peut déjà traiter d'autres personnes avec cette énergie.<br />Il continue son éducation à l'école et en même temps fait des études dans différents domaines. Il sera naturellement attiré par les arts martiaux et grâce à cela il apprendra beaucoup à propos de l'utilisation de l'énergie, du Ki.</p>
+                                <p class="text-muted mb-3">L'apprentissage spirituel et énergétique de Mikao Usui Sensei a été grandement influencé par 4 voies : le Shintoïsme, le Bouddhisme, les pratiques samouraïs et le Shugendo.</p>
+                                <p class="text-muted mb-3">Au cours de la Méditation de la repentance du lotus qui dure 3 semaines, Mikao Usui Sensei médite, chante, réalise les pratiques qui font parties de cette méditation. Et à un moment donné, il vit un moment hors du commun, un satori "illumination".<br />En rentrant chez lui, il sent qu'une nouvelle énergie passe par ses mains, il en est perplexe. Suite à cela il place ses mains sur son corps et se rend compte rapidement cela lui fait du bien et qu'il se sent mieux et de file en aiguille il se met à pratiquer l'imposition par les mains sur différentes personnes en obtenant des résultats positifs.</p>
+                                <p class="text-muted">Suite à cela Mikao Usui Sensei décida de quitter ses autres activités pour se consacrer à cette nouvelle découverte. Très rapidement il sera amené à fonder une organisation qui se nomme : Usui Reiki Ryoho Gakkaï et c'est dans ce cadre qu'il poursuivra cette nouvelle activité thérapeutique.</p>
                             </article>
                             <div class="col-md-6">
                                 <img class="img-fluid rounded-4" src="/../assets/img/services/Kenko-Ho/pageReiki/ReikiTitre.svg" alt="Image représantant une personne qui reçoit un soin Reiki">
@@ -42,22 +44,16 @@
                         <div class="row">
                             <article class="col-md-6">
                                 <h3 class="text-center my-4">En quoi consiste une séance de soin ?</h3>
-                                <p class="text-muted">La peau est l'organe le plus étendu du corps humain, notre peau possède plusieurs fonctions, elle joue le rôle de barrière (chocs, pollution, U.V, germes, ...), elle joue un rôle de régulation thermique, de synthèse hormonale (vit-D,...).</p>
-                                <p class="text-muted">La peau possède des fonctions au niveau immunitaire aussi. Qu'elle soit blanche ou noire, chaque peau est unique et réagira de façon différente. Avec les huiles essentielles vous pourrez aider votre peau dans différentes situations : (rougeurs, brûlures légères, sécheresse cutanée, taches de pigmentation, ...).
-                                </p>
+                                <p class="text-muted">Le receveur s'installera confortablement sur une table de soin, il restera habillé.<br />Le praticien passera ses mains à distance sur différentes zones du corps ce qui aura pour conséquence le rééquilibrage des structures énergétiques. Pour obtenir un résultat il faut parfois faire plusieurs séances rapprochées.</p>
+                                <p class="fw-bold my-2">Le Reiki ne se substitue en aucun cas à un traitement ni à un suivi médical si le patient requiert un suivi de ce type.</p>
                                 <div class="text-center my-4">
                                     <h3>Tarifs</h3>
                                 </div>
-                                <p class="text-muted">Téléchargez notre guide sur la peau et les huilles essentielles.</p>
+                                <p class="text-muted text-center">60.00 €/séance</p>
                             </article>
                             <div class="col-md-6">
                                 <img class="img-fluid rounded-4" src="/../assets/img/services/Kenko-Ho/pageReiki/Reiki.jpg" alt="Image représantant une personne qui reçoit un soin Reiki">
                             </div>
-                        </div>
-                        <div class="text-center my-5">
-                            <a href="/../assets/pdf/pageEnfants/enfants.pdf" download="Les HE & les enfants.pdf">
-                                <button class="button">Télécharger le PDF</button>
-                            </a>
                         </div>
                     </div>
                     <div class="text-center mt-auto">
