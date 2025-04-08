981b4e342a16988605123f0d80fcfd3f62e62139
Baboo81 <magicbaboo@gmail.com>
2025-04-08 13:44:23 +0200

    Reiki.php

--- App/Views/kenko-ho-themes/reiki.php
+++ App/Views/kenko-ho-themes/reiki.php
@@ -18,31 +18,40 @@
                         <div class="row">
                             <article class="col-md-6">
                                 <h3 class="text-center my-4">Qu'est-ce que le Reiki ?</h3>
+                                <p class="text-muted">Le Reiki est une méthode de soin énergétique d'origine japonaise.<br />Le Reiki est une pratique énergétique mais aussi une approche de développement spirituelle.</p>
+                                <p class="text-muted">Son fondateur ; Mikao Usui invitait ses élèves à appliquer 5 préceptes "Gokai" en japonais. Voici les 5 préceptes :
+                                </p>
+                                <ul>
+                                    <li>Juste pour aujourd'hui, je me libère de toute préocupation,</li>
+                                    <li>Juste pour aujourd'hui, je me libère de toute colère,</li>
+                                    <li>Juste pour aujourd'hui, je rends grâce pour mes nombreuses bénédictions, j'honore mes parents, mes professeurs et mes aïeux,</li>
+                                    <li>Juste pour aujourd'hui, je vis ma vie honnêtement,</li>
+                                    <li>Juste pour aujourd'hui, je respecte la vie sous toute ses formes.</li>
+                                </ul>
+                                <div class="text-center my-4">
+                                    <h3>Histoire du Reiki</h3>
+                                </div>
+                                <p class="text-muted">L'histoire du Reiki a commencé il y a près d'une centaine d'années au Japon grâce à Mikao Usui Sensei qui naquit le 15 août 1865. C'était un personnage relativement singulier, il est né dans une famille de samouraïs, même si ces derniers étaient déjà interdits à l'époque de son adolescence.<br />Une telle tradition ne s'oublie pas du jour au lendemain, probablement grâce à cela, dés sa prime jeunesse, il a commencé à s'intéresser à l'utilisation des énergies d'une façon générale et à la spiritualité.</p>
+                                <p class="text-muted">Vers l'âge de huit ans, il a commencé à pratiquer le Kiko (Qi Gong version japonaise). Au bout de 2 ou 3 ans, il acquiert des capacité énergétiques surprenantes et il peut déjà traiter d'autres personnes avec cette énergie.<br />Il continue son éducation à l'école et en même temps fait des études dans différents domaines. Il sera naturellement attiré par les arts martiaux et grâce à cela il apprendra beaucoup à propos de l'utilisation de l'énergie, du Ki.</p>
+                                <p class="text-muted">L'apprentissage spirituele et énergétique de Mikao Usui Sensei a été grandement influencé par 4 voies : le Shintoïsme, le Bouddhisme, les pratiques samouraïs et le Shugendo.</p>
+                            </article>
+                            <div class="col-md-6">
+                                <img class="img-fluid rounded-4" src="/../assets/img/services/Kenko-Ho/pageReiki/ReikiTitre.svg" alt="Image représantant une personne qui reçoit un soin Reiki">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <article class="col-md-6">
+                                <h3 class="text-center my-4">En quoi consiste une séance de soin ?</h3>
                                 <p class="text-muted">La peau est l'organe le plus étendu du corps humain, notre peau possède plusieurs fonctions, elle joue le rôle de barrière (chocs, pollution, U.V, germes, ...), elle joue un rôle de régulation thermique, de synthèse hormonale (vit-D,...).</p>
                                 <p class="text-muted">La peau possède des fonctions au niveau immunitaire aussi. Qu'elle soit blanche ou noire, chaque peau est unique et réagira de façon différente. Avec les huiles essentielles vous pourrez aider votre peau dans différentes situations : (rougeurs, brûlures légères, sécheresse cutanée, taches de pigmentation, ...).
                                 </p>
                                 <div class="text-center my-4">
-                                    <h3>Guide des huiles essentielles pour aider la peau</h3>
+                                    <h3>Tarifs</h3>
                                 </div>
                                 <p class="text-muted">Téléchargez notre guide sur la peau et les huilles essentielles.</p>
                             </article>
                             <div class="col-md-6">
                                 <img class="img-fluid rounded-4" src="/../assets/img/services/Kenko-Ho/pageReiki/Reiki.jpg" alt="Image représantant une personne qui reçoit un soin Reiki">
-                            </div>
-                        </div>
-                        <div class="row">
-                            <article class="col-md-6">
-                                <h3 class="text-center my-4">Qu'est-ce que le Reiki ?</h3>
-                                <p class="text-muted">La peau est l'organe le plus étendu du corps humain, notre peau possède plusieurs fonctions, elle joue le rôle de barrière (chocs, pollution, U.V, germes, ...), elle joue un rôle de régulation thermique, de synthèse hormonale (vit-D,...).</p>
-                                <p class="text-muted">La peau possède des fonctions au niveau immunitaire aussi. Qu'elle soit blanche ou noire, chaque peau est unique et réagira de façon différente. Avec les huiles essentielles vous pourrez aider votre peau dans différentes situations : (rougeurs, brûlures légères, sécheresse cutanée, taches de pigmentation, ...).
-                                </p>
-                                <div class="text-center my-4">
-                                    <h3>Guide des huiles essentielles pour aider la peau</h3>
-                                </div>
-                                <p class="text-muted">Téléchargez notre guide sur la peau et les huilles essentielles.</p>
-                            </article>
-                            <div class="col-md-6">
-                                <img class="img-fluid rounded-4" src="/../assets/img/services/Kenko-Ho/pageReiki/ReikiTitre.svg" alt="Image représantant une personne qui reçoit un soin Reiki">
                             </div>
                         </div>
                         <div class="text-center my-5">
