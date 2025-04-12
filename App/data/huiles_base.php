b1a90c77320fbaae8f6d3a24eea3cabbb5905453
Baboo81 <magicbaboo@gmail.com>
2025-04-12 08:04:10 +0200

    /data : huiles_base.php

--- App/data/huiles_base.php
+++ App/data/huiles_base.php
@@ -88,6 +88,18 @@ return [
                      Á éviter s\'il y a des allergies aux salicylés (aspirine).',
         'img' =>  './public/assets/img/services/Kenko-Ho/pageDixHuiles/DeepBlue.jpg'
     ],
+    'encens' => [
+        'nom' => 'L\'Encens (Frankincense)',
+        'usages' => 'Soutien cellulaire et des défenses naturelles.
+                     En diffusion, elle se marie bien avec toutes les huiles et procure un effet anxiolytique.
+                     En topique dans une crème, elle permet d\'éliminer les tâches, anti-rides (anti-oxydant).
+                     En interne : 1 gtte en bouche pour un effet relaxant.',
+        'img' =>  './public/assets/img/services/Kenko-Ho/pageDixHuiles/Encen.jpg'          
+    ],
+    'tea_tree' => [
+        'nom' => 'Le Teau-Tree',
+        'usages' => ''
+    ]
 ]
 
 ?>
