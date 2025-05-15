6bdd3769ab4bb1fe54204a7e0b0243f8480289c8
Baboo81 <magicbaboo@gmail.com>
2025-05-15 17:30:23 +0200

    contact.php

--- App/Views/contact.php
+++ App/Views/contact.php
@@ -24,26 +24,38 @@
                             <div class="col-md-6 col-12 my-5">  
                                 <div class="mb-3">
                                     <label for="name" class="form-label mb-3">Nom</label>
-                                    <input type="text" class="form-control" id="name" name="name" required>
+                                    <div class="input-group">
+                                        <span class="input-group-text"><i class="bi bi-person"></i></span>
+                                        <input type="text" class="form-control" id="name" name="name" placeholder="Votre nom de famille" required>
+                                    </div>
                                 </div>
                                 <div class="mb-3">
                                     <label for="firstName" class="form-label mb-3">Prénom</label>
-                                    <input type="text" class="form-control" id="firstName" name="firstName" required>
+                                    <div class="input-group">
+                                        <span class="input-group-text"><i class="bi bi-person"></i></span>
+                                        <input type="text" class="form-control" id="firstName" name="firstName" placeholder="Votre prénom" required>
+                                    </div>
                                 </div>
                                 <div class="mb-3">
                                     <label for="tel" class="form-label mb-3">Téléphone</label>
                                     <div class="input-group">
-                                        <span class="input-group-text"><i class="bi bi-telephone-fill"></i></span>
+                                        <span class="input-group-text"><i class="bi bi-telephone"></i></span>
                                         <input type="tel" class="form-control" id="tel" name="tel" placeholder="Votre numéro de téléphone" required>
                                     </div>
                                 </div>
                                 <div class="mb-3">
                                     <label for="email" class="form-label mb-3">Email</label>
-                                    <input type="email" class="form-control" id="email" name="email" required> 
+                                    <div class="input-group">
+                                        <span class="input-group-text"><i class="bi bi-envelope"></i></span>
+                                        <input type="email" class="form-control" id="email" name="email" required> 
+                                    </div>
                                 </div>
                                 <div class="mb-3">
                                     <label for="message" class="form-label mb-3">Message</label>
-                                    <textarea class="form-control" id="message" name="message" required></textarea>
+                                    <div class="input-group">
+                                        <span class="input-group-text"><i class="bi bi-chat-text"></i></span>
+                                        <textarea class="form-control" id="message" name="message" required></textarea>
+                                    </div>
                                 </div>
                                 <div class="text-center my-5">
                                     <button type="submit" class="button">Envoyer</button>
