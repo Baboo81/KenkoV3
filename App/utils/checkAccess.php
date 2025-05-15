ccaf34aea2ac055188c0ad3a7875bb80342b8b31
Baboo81 <magicbaboo@gmail.com>
2025-05-15 10:20:44 +0200

    Debug

--- App/utils/checkAccess.php
+++ App/utils/checkAccess.php
@@ -1,4 +1,8 @@
 <?php
+
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
     
     //Vérifie si l'utilisateur a bien entré le code
     if (!isset($_SESSION['kenko_access']) || $_SESSION['kenko_access'] !== true) {
