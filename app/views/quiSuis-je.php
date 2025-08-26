eb6414588de81d472c2d17dfe17f229d4cd5d4b9
Baboo81 <magicbaboo@gmail.com>
2025-08-26 13:46:26 +0200

    Corrections

--- app/views/quiSuis-je.php
+++ app/views/quiSuis-je.php
@@ -64,7 +64,7 @@
                         <article class="col-md-12 my-5 p-5">            
                             <div class="box rounded-5 p-5">
                                 <div class="col-md-12 d-flex align-items-center justify-content-center">
-                                    <img src="/assets/img/services/quiSuis-Je/parcours.svg" alt="Picto représentant un personnage montant des escaliers" style="height: 30rem;">
+                                    <img src="/assets/img/services/quiSuis-je/parcours.svg" alt="Picto représentant un personnage montant des escaliers" style="height: 30rem;">
                                 </div>  
                                 <p class="fontWhite mb-3">Après 15 années passées en pharmacie, j'ai décidé de réorienter ma carrière professionnelle pour aller vers une passion de longue date, l'informatique.<br />Après m'être formée au développement web, j'ai décidé de me lancer comme webmaster freelance.</p>
                                 <p class="fontWhite mb-3">La découverte de dōTERRA il y a quelques années m'a permis de m'orienter vers une autre passion, et cette passion c'est la santé. Avec ce grand interêt que je nourris pour la santé j'avais envie d'aider les personnes à améliorer leur bien-être avec d'une part les huiles essentielles mais aussi en partageant ce que j'ai pu experimenter au niveau de ma santé.</p>
